574daa8e22fee9bc681c76a4b8f4fb6ec506aaa4
Dennis Torres <djtorres0@gmail.com>
2017-07-11 06:57:24 -0400

    Remove previous lib folder on build

--- build.js
+++ build.js
@@ -3,7 +3,7 @@
 import asyncEach from 'async/each';
 import asyncParallel from 'async/parallel';
 import { readdir as fsReaddir } from 'fs';
-import { copy as fsExtraCopy } from 'fs-extra';
+import { copy as fsExtraCopy, remove as fsExtraRemove } from 'fs-extra';
 import _includes from 'lodash/fp/includes';
 import _map from 'lodash/fp/map';
 import { join as pathJoin } from 'path';
@@ -20,52 +20,58 @@ const build = (cb) => {
       return cb(readSrcDirErr);
     }
 
-    const srcLibs = _map(
-      srcFile => pathJoin(srcDir, srcFile),
-      srcFiles,
-    );
+    return fsExtraRemove(libDir, (removeLibDirErr) => {
+      if (removeLibDirErr) {
+        return cb(removeLibDirErr);
+      }
 
-    const bundlePlugins = [
-      rollupPluginFlow({ pretty: true }),
-      rollupPluginNodeResolve(),
-    ];
-
-    return asyncEach(srcFiles, (srcFile, done0) => {
-      const srcPath = pathJoin(srcDir, srcFile);
-      const libPath = pathJoin(libDir, srcFile);
-
-      const bundleExternal = (lib) => (
-        lib !== srcPath && _includes(lib, srcLibs)
+      const srcPaths = _map(
+        srcFile => pathJoin(srcDir, srcFile),
+        srcFiles,
       );
 
-      const bundleCreateOpts = {
-        entry: srcPath,
-        external: bundleExternal,
-        plugins: bundlePlugins,
-      };
+      const bundlePlugins = [
+        rollupPluginFlow({ pretty: true }),
+        rollupPluginNodeResolve(),
+      ];
 
-      const bundleWriteOpts = {
-        dest: libPath,
-        format: 'es',
-        sourceMap: true,
-      };
+      return asyncEach(srcFiles, (srcFile, done0) => {
+        const srcPath = pathJoin(srcDir, srcFile);
+        const libPath = pathJoin(libDir, srcFile);
 
-      asyncParallel([
-        done1 => (
-          fsExtraCopy(srcPath, `${libPath}.flow`)
-            .catch(err => done1(err))
-            .then(() => done1())
-        ),
-        done1 => (
-          rollupBundle(bundleCreateOpts)
-            .catch(err => done1(err))
-            .then((bundle) => (
-              bundle.write(bundleWriteOpts)
-                .catch(err => done1(err))
-                .then(() => done1())
-            ))
-        ),
-      ], done0);
+        const bundleExternal = (moduleId) => (
+          moduleId !== srcPath && _includes(moduleId, srcPaths)
+        );
+
+        const bundleCreateOpts = {
+          entry: srcPath,
+          external: bundleExternal,
+          plugins: bundlePlugins,
+        };
+
+        const bundleWriteOpts = {
+          dest: libPath,
+          format: 'es',
+          sourceMap: true,
+        };
+
+        asyncParallel([
+          done1 => (
+            fsExtraCopy(srcPath, `${libPath}.flow`)
+              .catch(err => done1(err))
+              .then(() => done1())
+          ),
+          done1 => (
+            rollupBundle(bundleCreateOpts)
+              .catch(err => done1(err))
+              .then((bundle) => (
+                bundle.write(bundleWriteOpts)
+                  .catch(err => done1(err))
+                  .then(() => done1())
+              ))
+          ),
+        ], done0);
+      });
     });
   });
 };
