b3d85b6f034a3d20bf57633a9ad2497f6e1d6596
Dennis Torres <djtorres0@gmail.com>
2017-08-10 10:29:35 -0400

    refactor(md_component_raised-button): shorten name of shadow export

--- src/component_raised-button/shadow_normal_default.js
+++ src/component_raised-button/shadow_normal_default.js
@@ -2,7 +2,7 @@
 
 import componentRaisedButtonElevationHeightNormal from './elevation_height_normal';
 import elevationShadowHeight2 from '../elevation/shadow_height_2_default';
-import get from './elevation_shadow_height_normal';
+import get from './shadow_normal';
 
 export default get({
   componentRaisedButtonElevationHeightNormal,
--- src/component_raised-button/shadow_pressed_default.js
+++ src/component_raised-button/shadow_pressed_default.js
@@ -2,7 +2,7 @@
 
 import componentRaisedButtonElevationHeightPressed from './elevation_height_pressed_default';
 import elevationShadowHeight8 from '../elevation/shadow_height_8_default';
-import get from './elevation_shadow_height_pressed';
+import get from './shadow_pressed';
 
 export default get({
   componentRaisedButtonElevationHeightPressed,
