504bc6c4ac20f05794887ace61c40c2f9bab0693
Dennis Torres <djtorres0@gmail.com>
2017-08-14 10:02:18 -0400

    feat(md_component_bottom-sheet): add bottom sheet component

---
+++ src/component_bottom-sheet/background-color_default.js
@@ -0,0 +1,6 @@
+// @flow
+
+import colorWhite from '../color/white_hsl';
+import get from './background-color';
+
+export default get({ colorWhite });
---
+++ src/component_bottom-sheet/overlay_color_default.js
@@ -0,0 +1,6 @@
+// @flow
+
+import colorBlack from '../color/black_hsl';
+import get from './overlay_color';
+
+export default get({ colorBlack });
